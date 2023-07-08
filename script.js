8234a405a14a8da29ecb2a4b8b675bb216c7bcd9
atamasnikita <116432849+atamasnikita@users.noreply.github.com>
2023-07-08 15:06:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ const button = document.querySelector('.toServer');
 button.addEventListener('click', ()=>{
     console.log('otpravka')
     fetch('http://localhost:8080/api/user',{
-        method: 'GET',
+        method: 'POST',
         headers: {
         'Content-Type': 'application/json;charset=utf-8'
         },
+
+        body: user
     })
     .then(response => console.log(response.json()))
 })
